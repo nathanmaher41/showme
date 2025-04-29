36300eaddd51b0aa69cf5cb9681a50db024afd66
Nathan Maher <nmaher@andrew.cmu.edu>
2025-04-29 14:44:00 -0700

    Initial Commit

---
+++ ShowMe/ShowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMeApp.swift
+//  ShowMe
+//
+//  Created by Nathan Maher on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShowMeTests/ShowMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMeTests.swift
+//  ShowMeTests
+//
+//  Created by Nathan Maher on 4/29/25.
+//
+
+import Testing
+@testable import ShowMe
+
+struct ShowMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
